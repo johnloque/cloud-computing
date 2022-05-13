501406c2969a80f919b7ff6c1588a7a50d73ec67
Michel Capot <michel-capot@live.fr>
2022-05-13 13:03:53 +0200

    Finsh game implementation

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ db = create_db()
 n_trials = 10
 target_list = []
 guessed_letters = []
+win = False
 
 #endpoints
 
@@ -54,23 +55,29 @@ def play_hangman():
     global n_trials
     global target_list
     global guessed_letters
+    global win
 
     if request.method == 'GET':
         new_word_id = draw_word()
         new_word = db[new_word_id]['words']
+        db[new_word_id]['counts'] += 1
+        n_trials = 10
         target_list = [char for char in new_word]
         guessed_letters = []
+        win = False
         masked_word = mask_word(target_list, guessed_letters)
-        return render_template('play.html', n_trials = n_trials, new_word=masked_word, field_name='Suggest a new character : ')
+        return render_template('play.html', n_trials = n_trials, new_word=masked_word, field_name='Suggest a new character : ', win=win)
 
     elif request.method == 'POST':
         new_letter = request.form['guess'].lower()
         if new_letter in guessed_letters :
             masked_word = mask_word(target_list, guessed_letters)
             field_name = "You have already suggested this letter. Suggest another one : "
-            return render_template('play.html', n_trials = n_trials, new_word=masked_word, field_name=field_name)
+            return render_template('play.html', n_trials = n_trials, new_word=masked_word, field_name=field_name, win=win)
         else :
             guessed_letters.append(new_letter)
             masked_word = mask_word(target_list, guessed_letters)
             n_trials -= 1
-            return render_template('play.html', n_trials = n_trials, new_word=masked_word, field_name='Suggest a new character : ')
+            if all(char in guessed_letters for char in list(set(target_list))) :
+                win = True
+            return render_template('play.html', n_trials = n_trials, new_word=masked_word, field_name='Suggest a new character : ', win=win)
